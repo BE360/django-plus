7f4efb64e76d583ceccd92d5a0a8027945290ada
Reza Abbasi <reza.abbasi95@gmail.com>
2018-03-06 15:31:44 +0330

    url title limit

--- django_plus/admin/field/url.py
+++ django_plus/admin/field/url.py
@@ -2,7 +2,7 @@ from ._generator_ import admin_field_generator
 from django_plus.admin.html_tags import anchor_tag
 
 
-def url(verbose_name="", title=None, path_to_field=None):
+def url(verbose_name="", title=None, path_to_field=None, title_limit=-1):
     """
     Function should return a url link
     :param verbose_name:
@@ -21,6 +21,9 @@ def url(verbose_name="", title=None, path_to_field=None):
                 url_title = url_title[:-1]
         else:
             url_title = title
+
+        if title_limit > 0 and len(url_title) > title_limit:
+            url_title = url_title[:title_limit] + '...'
 
         return anchor_tag(url_title, link, target='_blank')
 
