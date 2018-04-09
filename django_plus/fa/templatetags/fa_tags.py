8d7d81e77abc57a5b0520216ef641f1c857a6546
Reza Abbasi <reza.abbasi95@gmail.com>
2018-04-09 11:57:04 +0430

    fa_tags templatetags created

---
+++ django_plus/fa/templatetags/fa_tags.py
@@ -0,0 +1,20 @@
+import datetime
+
+import jdatetime
+from django import template
+
+from django_plus.fa import fa_convert
+
+register = template.Library()
+
+
+@register.filter(name='fa_date')
+def fa_date(value, date_format='%Y-%m-%d'):
+
+    if isinstance(value, datetime.date):
+        value = jdatetime.date.fromgregorian(date=value)
+
+    if not isinstance(value, datetime.date):
+        raise Exception("Value type is not valid.")
+
+    return fa_convert(value.strftime(date_format))
