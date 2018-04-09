399caa3e5c1369449165eda29970c86b83625fba
Reza Abbasi <reza.abbasi95@gmail.com>
2018-04-09 12:25:23 +0430

    fa_currency templatetag added

--- django_plus/fa/templatetags/fa_tags.py
+++ django_plus/fa/templatetags/fa_tags.py
@@ -3,18 +3,28 @@ import datetime
 import jdatetime
 from django import template
 
-from django_plus.fa import fa_convert
+from django_plus.fa import fa_convert, fa_currency
 
 register = template.Library()
 
 
 @register.filter(name='fa_date')
-def fa_date(value, date_format='%Y-%m-%d'):
+def fa_date_filter(value, date_format='%Y-%m-%d'):
 
     if isinstance(value, datetime.date):
         value = jdatetime.date.fromgregorian(date=value)
 
-    if not isinstance(value, datetime.date):
+    if not isinstance(value, jdatetime.date):
         raise Exception("Value type is not valid.")
 
     return fa_convert(value.strftime(date_format))
+
+
+@register.filter(name='fa_currency')
+def fa_currency_filter(value):
+
+    if isinstance(value, int):
+        return fa_currency(value)
+
+    else:
+        return value
