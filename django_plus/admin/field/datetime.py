90435dad518abed2646d223a83fee9e5660dab8b
Reza Abbasi <reza.abbasi95@gmail.com>
2018-04-17 17:51:33 +0430

    datetime field bug fixed

--- django_plus/admin/field/datetime.py
+++ django_plus/admin/field/datetime.py
@@ -1,6 +1,21 @@
-from django_plus.admin.field import date
+from django_plus.fa.templatetags.fa_tags import fa_datetime_filter
+from ._generator_ import admin_field_generator
 
 
 def datetime(verbose_name="", path_to_field=None, time_format="%Y/%m/%d ساعت %H:%i:%s",
              wrap_white_space=True):
-    return date(verbose_name, path_to_field, time_format, wrap_white_space)
+
+    def function_changer(func, admin, instance):
+
+        result_date = func(admin, instance)
+        if result_date:
+            return fa_datetime_filter(result_date, time_format)
+        else:
+            return ""
+
+    return admin_field_generator(
+        verbose_name=verbose_name,
+        path_to_field=path_to_field,
+        function_changer=function_changer,
+        wrap_white_space=wrap_white_space
+    )
