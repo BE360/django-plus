337f7ddb3bbf20e7137da2f7e5c86e87b82d732b
Reza Abbasi <reza.abbasi95@gmail.com>
2018-04-17 17:41:58 +0430

    bug fixed in datetime admin field

--- django_plus/admin/field/datetime.py
+++ django_plus/admin/field/datetime.py
@@ -1,7 +1,6 @@
 from django_plus.admin.field import date
-from django_plus.fa import CUSTOM_DATETIME_FORMAT
 
 
-def datetime(verbose_name="", path_to_field=None, time_format=CUSTOM_DATETIME_FORMAT,
+def datetime(verbose_name="", path_to_field=None, time_format="%Y/%m/%d ساعت %H:%i:%s",
              wrap_white_space=True):
     return date(verbose_name, path_to_field, time_format, wrap_white_space)
