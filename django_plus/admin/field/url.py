e37caf3914f89c9e2ef5eaafbe95bb7122014e61
Reza Abbasi <rz.abs.shi@gmail.com>
2018-05-15 16:22:57 +0430

    hide_on_null_link added to url field

--- django_plus/admin/field/url.py
+++ django_plus/admin/field/url.py
@@ -2,15 +2,21 @@ from ._generator_ import admin_field_generator
 from django_plus.admin.html_tags import anchor_tag
 
 
-def url(verbose_name="", title=None, path_to_field=None, limit=-1, wrap_white_space=True):
+def url(verbose_name="", title=None, path_to_field=None, limit=-1, wrap_white_space=True, hide_on_null_link=True):
     """
     Function should return a url link
     :param verbose_name:
     :param title: url title. if not presented we use the link as title as well
     :param path_to_field:
+    :param limit:
+    :param wrap_white_space:
+    :param hide_on_null_link:
     """
     def function_changer(func, admin, instance):
         link = func(admin, instance)
+
+        if not link and hide_on_null_link:
+            return ''
 
         if not link.startswith('http://') and not link.startswith('https://'):
             link = 'http://' + link
@@ -22,7 +28,7 @@ def url(verbose_name="", title=None, path_to_field=None, limit=-1, wrap_white_sp
         else:
             url_title = title
 
-        if limit >= 0 and len(url_title) > limit:
+        if len(url_title) > limit >= 0:
             url_title = url_title[:limit] + '...'
 
         return anchor_tag(url_title, link, target='_blank')
